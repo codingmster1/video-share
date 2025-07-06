cfee8d5708d2d37abfb96e312d3c99f99e5e5dd2
Jeffrey Doyle <doylejeff98@gmail.com>
2025-07-06 14:36:22 -0400

    dropdown hamburger menu

--- components/DropdownList.tsx
+++ components/DropdownList.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+'use client';
+import Image from 'next/image';
+import React, { useState } from 'react'
 
 const DropdownList = () => {
+    const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>DropdownList</div>
+    <div className="relative">
+        <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+
+<div className="filter-trigger">
+<figure>
+    <Image src="/assets/icons/hamburger.svg" alt="menu"
+    width={14} height={14} />
+</figure>
+</div>
+
+        </div>
+        </div>
   )
 }
 
