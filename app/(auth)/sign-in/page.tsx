5ad77152f0d413b451ff6dcaeeeff5abc9fa16b4
Jeffrey Doyle <doylejeff98@gmail.com>
2025-07-07 15:17:33 -0400

    h2 text with logos and star rating

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,6 +10,27 @@ const page = () => {
 <Image src="/assets/icons/logo.svg" alt="logo" width={32} height={32} />
 <h1>SnapCast</h1>
 </Link>
+<div className="description">
+<section>
+  <figure>
+    {Array.from({ length: 5}).map((_, index) => (
+      <Image src="/assets/icons/star.svg" alt="star" width={20}
+      height={20} key={index} />
+    ))}
+  </figure>
+  <p>SnapCast makes screen recording simple like it should be!</p>
+  <article>
+    <Image src="/assets/images/jason.png" alt="jason" width={64} height={64}
+    className="rounded-full"/>  
+    <div>
+      <h2>
+        Jason Rivera
+      </h2>
+      <p>Product Designer</p>
+    </div>
+    </article>
+</section>
+</div>
       </aside>
     </main>
   )
