942759ff51d986712c4c7a41df7351e946dbd324
Jeffrey Doyle <doylejeff98@gmail.com>
2025-07-07 15:09:35 -0400

    logo imported and rendered on sign in route

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Auth</div>
+    <main className="sign-in">
+      <aside className="testimonial">
+<Link href="/">
+<Image src="/assets/icons/logo.svg" alt="logo" width={32} height={32} />
+<h1>SnapCast</h1>
+</Link>
+      </aside>
+    </main>
   )
 }
 
