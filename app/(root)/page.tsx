a1d9590a7173ff4aa8fe704423c33befe0c3442d
Jeffrey Doyle <doylejeff98@gmail.com>
2025-07-07 15:02:38 -0400

    video details page route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,11 +7,13 @@ const Page = () => {
   return (
     <main className="wrapper page">
       <Header title="All Videos" subHeader="Public Library"/>
-
+      
+      <section className="video-grid">
 {dummyCards.map((card) => (
   <VideoCard key={card.id}{...card} />
 ))}
 
+</section>
       </main>
   )
 }
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Header from '@/components/Header';
+import VideoCard from '@/components/VideoCard';
+import { dummyCards } from '@/constants';
 import React from 'react'
 
 const page = async({params}: ParamsWithSearch) => {
@@ -7,7 +9,13 @@ const page = async({params}: ParamsWithSearch) => {
     <div className="wrapper page">
         <Header subHeader="doylejeff98@gmail.com" title="Jeff"
         userImg="/assets/images/dummy.jpg"/>
-        <h1 className="text-2xl font-karla">USER ID: {id}</h1>
+       
+        <section className="video-grid">
+        {dummyCards.map((card) => (
+  <VideoCard key={card.id}{...card} />
+))}
+        </section>
+
         </div>
   )
 }
