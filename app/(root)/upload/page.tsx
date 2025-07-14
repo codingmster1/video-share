2997db766935a4de5218f3d12ad7fa028362e70b
Jeffrey Doyle <doylejeff98@gmail.com>
2025-07-14 17:32:52 -0400

    title and description input boxes

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 import FileInput from '@/components/FileInput'
 import FormField from '@/components/FormField'
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 const page = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        visibility: 'public',
+    })
     const [error, setError] = useState(null)
+
+    const handleInputChange = (e: ChangeEvent) => {
+const {name, value } = e.target;
+
+setFormData((prevState) => ({ ...prevState, [name]: value}))
+    }
+
+
   return (
     <div className="wrapper-md upload-page">
         <h1>Upload a Video</h1>
@@ -12,10 +25,24 @@ const page = () => {
 
         <form className="rounded-20 shadow-10 gap-6 w-full flex
         flex-col px-5 py-7.5">
-
-        </form>
-<FormField/>
+<FormField
+id="title"
+label="Title"
+value={formData.title}
+onChange={handleInputChange}
+placeholder="Enter a title for your video"
+/>
+<FormField
+id="description"
+label="Description"
+value={formData.description}
+onChange={handleInputChange}
+as="textarea"
+placeholder="Description"
+/>
 <FileInput/>
+        </form>
+
     </div>
   )
 }
